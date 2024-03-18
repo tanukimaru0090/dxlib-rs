ae793469fa2e938fb78d1c22590095e3d0333476
Tanukimaru0090 <TANUSAN1015@gmail.com>
2024-03-19 06:13:32 +0900

    Update

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     let dll_name = "DxLib_x64.dll";
 
     // 利用者側のプロジェクトのディレクトリパスを取得
-    let project_dir = env::var("OUT_DIR").unwrap();
+    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let project_path = PathBuf::from(project_dir);
 
     // DLLのパスを構築
