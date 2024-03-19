fb9a03c1b7421a224925bc321a634c598d290fd2
Tanukimaru0090 <TANUSAN1015@gmail.com>
2024-03-20 00:59:19 +0900

    Update

--- build.rs
+++ build.rs
@@ -6,12 +6,9 @@ use std::path::PathBuf;
 fn main()->Result<(),Box<dyn std::error::Error>> {
     let env_name = "DXLIB_DLL";
     let dir = env::var(env_name);
-    if let Some(val) = dir.ok(){
-            if let Some(out) = env::var("OUT_DIR").ok(){
-                std::fs::copy(env_name, out)?;
-            }
-    }else{
-        panic!("環境変数{:}が設定されていません。",env_name);
+    if let Some(err) = dir.err(){
+          println!("{:}",err);
+          println!("環境変数{:}が設定されていません。",env_name);
     }
     Ok(())
 }
